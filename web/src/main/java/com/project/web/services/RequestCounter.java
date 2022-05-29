f00979e777827d5fb2f64a200a5b58d5ab2b56ea
Alexander <alexandrov.alexandder@gmail.com>
2022-05-29 22:03:23 +0300

    Some new features

--- web/src/main/java/com/project/web/services/RequestCounter.java
+++ web/src/main/java/com/project/web/services/RequestCounter.java
@@ -1,16 +1,23 @@
 package com.project.web.services;
 
 import org.springframework.stereotype.Service;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class RequestCounter {
+    static private ReentrantLock mutex = new ReentrantLock();
+
     private static int counter;
 
     public RequestCounter() {
         counter = 0;
     }
 
-    public static void increase() { counter++; }
+    public static void increase() {
+        mutex.lock();
+        counter++;
+        mutex.unlock();
+    }
 
     public static Integer getCounter() { return counter; }
 }
